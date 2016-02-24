cb68503f7d2367aeb553c93b07550cdab188f772
jenkins <jenkins@teamw.in>
2016-02-24 16:16:37 -0500

    Fixes

--- cm.mk
+++ cm.mk
@@ -1,18 +1,18 @@
 ## Specify phone tech before including full_phone	
-$(call inherit-product, vendor/aicp/configs/gsm.mk)
+$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := vivalto3gvn
 
 # Inherit some common aicp stuff.
-$(call inherit-product, vendor/aicp/configs/common.mk)
+$(call inherit-product, vendor/cm/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/device_vivalto3gvn.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vivalto3gvn
-PRODUCT_NAME := aicp_vivalto3gvn
+PRODUCT_NAME := cm_vivalto3gvn
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G313HZ
 PRODUCT_MANUFACTURER := samsung
